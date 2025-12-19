b8b86783b73407b93e637de9f4a81a4bf8235cfd
Nathan Zeng <nathan.j.zeng@gmail.com>
2025-12-18 19:25:39 -0800

    maintain the component theme but keep my font

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,7 @@ import App from './App';
 import './index.css';
 import { PrimeReactProvider } from 'primereact/api';
 import 'primeicons/primeicons.css';
-// TODO: The components don't look right without this, but this also changes the font of the app
-// import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
