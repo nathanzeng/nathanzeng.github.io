e569933fdad580cd2901e6122961e640749da691
Nathan Zeng <nathan.j.zeng@gmail.com>
2025-12-20 11:09:14 -0800

    wip: mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import RiceBowl from '../assets/rice_favicon/android-chrome-512x512.png';
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
 
-const Navbar: React.FC = () => {
+const DesktopNavbar: React.FC = () => {
   const location = useLocation();
 
   const isActive = (path: string) =>
@@ -15,6 +17,7 @@ const Navbar: React.FC = () => {
       <Link to="/">
         <img src={RiceBowl} alt="home button" className="w-10 h-auto" />
       </Link>
+
       <nav className="flex gap-5 p-4 justify-center flex-1">
         <Link to="/" className={isActive('/')}>
           Home
@@ -22,12 +25,71 @@ const Navbar: React.FC = () => {
         <Link to="/games" className={isActive('/games')}>
           Games
         </Link>
-        <Link to="/compoundInterestCalculator" className={isActive('/compoundInterestCalculator')}>
+        <Link
+          to="/compoundInterestCalculator"
+          className={isActive('/compoundInterestCalculator')}
+        >
           Calculator
         </Link>
       </nav>
     </div>
   );
 };
+
+function MobileNavbar() {
+  const [visible, setVisible] = useState<boolean>(false);
+  const isActive = (path: string) =>
+    location.pathname === path
+      ? 'bg-gray-300 p-3 rounded'
+      : 'p-3 rounded hover:text-font-primary-dark transition-colors';
+
+  return (
+    <div className="card flex justify-content-center">
+      <Sidebar visible={visible} onHide={() => setVisible(false)}>
+        <nav className="flex flex-col gap-5 p-4 justify-center">
+          <Link
+            to="/"
+            className={isActive('/')}
+            onClick={() => setVisible(false)}
+          >
+            Home
+          </Link>
+          <Link
+            to="/games"
+            className={isActive('/games')}
+            onClick={() => setVisible(false)}
+          >
+            Games
+          </Link>
+          <Link
+            to="/compoundInterestCalculator"
+            className={isActive('/compoundInterestCalculator')}
+            onClick={() => setVisible(false)}
+          >
+            Calculator
+          </Link>
+        </nav>
+      </Sidebar>
+
+      {/* TODO: I want this to not be blue */}
+      {/* TODO: Alignment looks jank */}
+      <Button rounded text icon="pi pi-bars" onClick={() => setVisible(true)} />
+      <img src={RiceBowl} alt="home button" className="w-10 h-10" />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <>
+      <div className="hidden md:block">
+        <DesktopNavbar />
+      </div>
+      <div className="md:hidden">
+        <MobileNavbar />
+      </div>
+    </>
+  );
+}
 
 export default Navbar;
